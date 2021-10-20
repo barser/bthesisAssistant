8eafc09bb04ecae6ce103ac55cf9f8d37083fcba
barser <sb@barser.ru>
2021-10-20 15:58:53 +0300

    Added Version by ldflags.

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Custom assistant for Thesis
 
 ```shell
 GOOS=windows go build -ldflags -H=windowsgui && mv bthesisAssistant.exe ~/Share
+go build -v -ldflags="-X 'main.Version=0.1-linux'" ./cmd/bthesisAssistant/
 ```
 
--- cmd/bthesisAssistant/main.go
+++ cmd/bthesisAssistant/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"bthesisAssistant/internal/notification"
 	"bthesisAssistant/internal/tray"
+	"fmt"
+	"log"
+	"os"
 )
+
+var Version = "unversioned"
 
 func main() {
 	go func() {
 		notification.Inform("Привет, мир!")
 	}()
+
+	_, err := fmt.Fprintln(os.Stdout, "Version:\t", Version)
+	if err != nil {
+		log.Println(err)
+	}
+
 	tray.Run()
 }
