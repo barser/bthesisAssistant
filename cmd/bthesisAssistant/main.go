114eedc4f22425d7b56a9896907f1f03b3fffb1f
barser <sb@barser.ru>
2021-10-22 08:04:26 +0300

    Chans and timers

--- cmd/bthesisAssistant/main.go
+++ cmd/bthesisAssistant/main.go
@@ -6,19 +6,44 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 var Version = "unversioned"
 
 func main() {
-	go func() {
+	cc := make(chan int64, 1)
+	tc := make(chan time.Time, 1)
+	go func(tc <-chan time.Time, cc chan<- int64) {
 		notification.Inform("Привет, мир!")
-	}()
+		var i int64 = 0
+		for {
+			select {
+			case <-tc:
+				notification.Inform("Пришло событие от таймера")
+				i++
+				cc <- i
+			}
+		}
+	}(tc, cc)
+	tc <- time.Now()
 
 	_, err := fmt.Fprintln(os.Stdout, "Version:\t", Version)
 	if err != nil {
 		log.Println(err)
 	}
 
-	tray.Run()
+	go tray.Run()
+
+	for {
+		select {
+		case v := <-cc:
+			if v > 10 {
+				os.Exit(0)
+			} else {
+				time.Sleep(2 * time.Second)
+				tc <- time.Now()
+			}
+		}
+	}
 }
